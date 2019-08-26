9226ad1eaec7bae8ae7c233ca39865689731b609
Scott Xu <jianx2@cisco.com>
2019-08-26 10:59:13 +0800

    reformat

--- 第一章 Java编程基础/018-循环和判断的总结/code018/src/Comments.java
+++ 第一章 Java编程基础/018-循环和判断的总结/code018/src/Comments.java
@@ -1,6 +1,7 @@
 public class Comments {
     public static void main(String[] args) {
-        int a = 9;//定义变量a，赋值为9
+        //定义变量a，赋值为9
+        int a = 9;
         //将a输出到控制台
         System.out.println(a);
     }
